5921a9f405580f41a0f5d415d7d5e94552ee0583
Gaetan Bloch <gaetan.bloch@gmail.com>
2020-05-28 01:03:01 +0200

    The "this" keyword inside of the anonymous function called setTimeout refers to the Window Object

--- special-cases.js
+++ special-cases.js
@@ -3,3 +3,8 @@ button.addEventListener('click', function () {
   // Refers to the button element
   console.log(this);
 });
+
+setTimeout(function () {
+  // Refers to the Window Object
+  console.log(this);
+}, 1000)
