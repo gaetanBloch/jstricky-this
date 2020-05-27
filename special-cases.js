a3b69cfd1c907b9dc8cdf6b86a5e57ff47e79597
Gaetan Bloch <gaetan.bloch@gmail.com>
2020-05-28 01:18:17 +0200

    Using the "this" keyword inside a class and see the problem when using the setTimeout

--- special-cases.js
+++ special-cases.js
@@ -1,10 +1,31 @@
+class Person {
+  constructor() {
+    this.age = 31
+  }
+
+  greet() {
+    console.log(this);
+    console.log('My age: ' + this.age);
+  }
+
+  greetWithDelay() {
+    // the "this" in the greet method passed doesn't have the same context
+    // as the class but the Window Object
+    setTimeout(this.greet, 1500);
+  }
+}
+
+const gaetan = new Person();
+gaetan.greet();
+gaetan.greetWithDelay();
+
 const button = document.querySelector('button');
 button.addEventListener('click', function () {
   // Refers to the button element
   console.log(this);
 });
 
-setTimeout(function () {
-  // Refers to the Window Object
-  console.log(this);
-}, 1000)
+// setTimeout(function () {
+//   // Refers to the Window Object
+//   console.log(this);
+// }, 1000);
