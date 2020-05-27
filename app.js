9261e0de6d273154bb12360eecefec9661a9987e
Gaetan Bloch <gaetan.bloch@gmail.com>
2020-05-28 00:55:20 +0200

    How "this" works

--- app.js
+++ app.js
@@ -1,9 +1,21 @@
-'use strict';
-
-console.log(this); // Window Object in the Browser
-
-greet();
+this.age = 31;
 
 function greet() {
-  console.log(this); // Yields undefined in strict mode
+  console.log(`I am ${this.age} years old`);
 }
+greet(); // I am 31 years old
+
+// "this" refers to the object on which a function (method) i called.
+// Hence this is defined by the execution context.
+const person = { age: 50, greet };
+person.greet(); // I am 50 years old
+
+// 'use strict';
+//
+// console.log(this); // Window Object in the Browser
+//
+// greet();
+//
+// function greet() {
+//   console.log(this); // Yields undefined in strict mode
+// }
